2d3e1a2dcc536efb0fb785084f4c347cf946a5ce
Gabriel Lima Gomes <gabriellimagomes.14@hotmail.com>
2021-03-10 20:45:48 -0300

    criação de domínio simples de cliente

---
+++ DDD/DDD/Domain/Entities/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class Cliente
+    {
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Apelido { get; private set; }
+        public DateTime DataNascimento { get; private set; }
+
+        public Cliente(Guid? id, string nome, string apelido, DateTime dataNascimento)
+        {
+            Id = id ?? Guid.NewGuid();
+            Nome = nome ?? throw new ArgumentNullException(nameof(nome));
+            Apelido = apelido ?? throw new ArgumentNullException(nameof(apelido));
+            DataNascimento = dataNascimento > DateTime.Now ?
+                throw new ApplicationException("A data de nascimento não pode ser maior que a data atual")
+                : dataNascimento;
+        }
+
+        public void TrocarApelido(string apelido)
+        {
+            Apelido = apelido ?? throw new ArgumentNullException(nameof(apelido));
+        }
+    }
+}
---
+++ DDD/DDD/README.md
@@ -0,0 +1,7 @@
+#DDD (Domain Driven Design)
+
+O DDD tem 3 conceitos base
+
+- Bounded Context
+- Context Mapping
+- Ubiquos Language
